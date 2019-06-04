517ed2f18e21fd9a048b26a1c1e6970bc7be7901
Cisco89 <franciscoadriangarcia1@gmail.com>
2019-06-03 20:26:12 -0600

    Changing the repo to ssh

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import datetime
 from authentication import api
 
+# testing if ssh now works
+
 message = "boop boop beep ... WAAALLLLEEEE"
 # message = "The time is {}. Do you know where your twitter bot is?".format(
     # datetime.datetime.now().strftime('%-I:%m %p'))
