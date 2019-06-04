6b9d5106b64b5ce9898c1765f73ed5e0cc29f0f9
Cisco89 <franciscoadriangarcia1@gmail.com>
2019-06-03 20:19:43 -0600

    Bot says something more unique

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import datetime
 from authentication import api
 
-message = "The time is {}. Do you know where your twitter bot is?".format(
-    datetime.datetime.now().strftime('%-I:%m %p'))
+message = "boop boop beep ... WAAALLLLEEEE"
+# message = "The time is {}. Do you know where your twitter bot is?".format(
+    # datetime.datetime.now().strftime('%-I:%m %p'))
 print('posting this clever message to twitter:')
 print(message)
 
